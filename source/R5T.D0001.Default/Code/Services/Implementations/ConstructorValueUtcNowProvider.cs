790481ecffcbfd4c13a6dee2c769dacd3c87b469
MinexAutomation <minex.automation@gmail.com>
2020-05-14 23:44:18 -0700

    Changed name of ConstructorValueUtcNowProvider. Added StaticValueUtcNowProvider.

---
+++ source/R5T.D0001.Default/Code/Services/Implementations/ConstructorValueUtcNowProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0001.Default
+{
+    public class ConstructorValueUtcNowProvider : IUtcNowProvider
+    {
+        #region Static
+
+        public static ConstructorValueUtcNowProvider NewFromUtcNow(DateTime utcNow)
+        {
+            var constantValueUtcNowProvider = new ConstructorValueUtcNowProvider(utcNow);
+            return constantValueUtcNowProvider;
+        }
+
+        public static ConstructorValueUtcNowProvider New(DateTime utcNow)
+        {
+            var constantValueUtcNowProvider = ConstructorValueUtcNowProvider.NewFromUtcNow(utcNow);
+            return constantValueUtcNowProvider;
+        }
+
+        public static ConstructorValueUtcNowProvider NewFromLocalNow(DateTime localNow)
+        {
+            var utcNow = localNow.ToUniversalTime();
+
+            var constantValueUtcNowProvider = new ConstructorValueUtcNowProvider(utcNow);
+            return constantValueUtcNowProvider;
+        }
+
+        public static ConstructorValueUtcNowProvider NewFromOffset(TimeSpan offset)
+        {
+            var offsetUtcNow = DateTime.UtcNow + offset;
+
+            var constantValueUtcNowProvider = ConstructorValueUtcNowProvider.NewFromUtcNow(offsetUtcNow);
+            return constantValueUtcNowProvider;
+        }
+
+        #endregion
+
+
+        private DateTime UtcNow { get; }
+
+
+        public ConstructorValueUtcNowProvider(DateTime utcNow)
+        {
+            this.UtcNow = utcNow;
+        }
+
+        public Task<DateTime> GetUtcNowAsync()
+        {
+            return Task.FromResult(this.UtcNow);
+        }
+    }
+}
---
+++ source/R5T.D0001.Default/Code/Services/Implementations/StaticValueUtcNowProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0001.Default
+{
+    public class StaticValueUtcNowProvider : IUtcNowProvider
+    {
+        /// <summary>
+        /// Note: not thread-safe.
+        /// </summary>
+        public static DateTime UtcNow { get; set; }
+
+
+        public Task<DateTime> GetUtcNowAsync()
+        {
+            return Task.FromResult(StaticValueUtcNowProvider.UtcNow);
+        }
+    }
+}
