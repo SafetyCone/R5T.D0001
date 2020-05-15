8e6b75ba6ae9e5d45d1aa6fca12c91d1674601c2
MinexAutomation <minex.automation@gmail.com>
2020-05-14 23:31:58 -0700

    Fixed lack of -Async suffix on async method.

--- source/R5T.D0001.Base/Code/Services/Extensions/IUtcNowProviderExtensions.cs
+++ source/R5T.D0001.Base/Code/Services/Extensions/IUtcNowProviderExtensions.cs
@@ -6,7 +6,7 @@ namespace R5T.D0001
 {
     public static class IUtcNowProviderExtensions
     {
-        public static async Task<DateTime> GetNow(IUtcNowProvider utcNowProvider)
+        public static async Task<DateTime> GetNowAsync(IUtcNowProvider utcNowProvider)
         {
             var utcNow = await utcNowProvider.GetUtcNowAsync();
 
