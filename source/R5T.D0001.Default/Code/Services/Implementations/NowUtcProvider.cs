6d495616592b0ca13ba3c242acdbd110f0f87fb5
davidcoats <david@getsafetycone.com>
2023-02-10 17:39:54 -0800

    Interim changes.

--- source/R5T.D0001.Default/Code/Services/Implementations/NowUtcProvider.cs
+++ source/R5T.D0001.Default/Code/Services/Implementations/NowUtcProvider.cs
@@ -3,10 +3,11 @@ using System.Threading.Tasks;using R5T.T0064;
 
 
 namespace R5T.D0001.Default
-{[ServiceImplementationMarker]
+{
     /// <summary>
     /// Returns the <see cref="DateTime.UtcNow"/> value.
     /// </summary>
+    [ServiceImplementationMarker]
     public class NowUtcProvider : INowUtcProvider,IServiceImplementation
     {
         public Task<DateTime> GetNowUtc()
